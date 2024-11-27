4562838dd3ef268d5291d285f5f6ea833ad3a896
upar1234 <upar1234@gmail.com>
2024-11-27 10:42:17 -0500

    a

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -7,39 +7,39 @@ import pandas as pd  # type: ignore
 from homework import clean_data
 
 
-def test_01():
+def test_homework():
     """Test homework/clean_data.py"""
 
     clean_data.main(
         "files/input/input.txt",
-        "files/output/output.txt",
+        "files/output.txt",
     )
 
-    if not os.path.exists("files/output/test.csv"):
-        raise FileNotFoundError("File 'files/output/test.csv' not found")
+    if not os.path.exists("files/test.csv"):
+        raise FileNotFoundError("File 'files/test.csv' not found")
 
     test = pd.read_csv("files/test.csv", index_col=None)
 
-    assert test.loc[0, "key"] == "acdeghinoqruy"
-    assert test.loc[1, "key"] == "acdegilmnoty"
-    assert test.loc[3, "key"] == "acdehioqrsu"
-    assert test.loc[6, "key"] == "acdehoqruy"
-    assert test.loc[12, "key"] == "acdeilmnoty"
-    assert test.loc[16, "key"] == "acdgilnoprstu"
+    assert test.loc[0, "key"] == "adhoc queri"
+    assert test.loc[6, "key"] == "agricultur product"
+    assert test.loc[11, "key"] == "airlin"
+    assert test.loc[12, "key"] == "airlin compani"
+    assert test.loc[16, "key"] == "analyt applic"
+    assert test.loc[25, "key"] == "analyt model"
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("files/output/output.txt"):
-        raise FileNotFoundError("File 'files/output/output.txt' not found")
+    if not os.path.exists("files/output.txt"):
+        raise FileNotFoundError("File 'files/output.txt' not found")
 
     #
     # Lee el contenido del archivo output.txt
-    dataframe = pd.read_csv("files/output/output.txt")
+    dataframe = pd.read_csv("files/output.txt")
     count = dataframe.groupby("text").size()
 
-    assert count.loc["AD-HOC QUERIES"] == 3
-    assert count.loc["AGRICULTURAL PRODUCTION"] == 1
-    assert count.loc["AIRLINE COMPANIES"] == 1
+    assert count.loc["AD-HOC QUERIES"] == 6
+    assert count.loc["AGRICULTURAL PRODUCTION"] == 5
+    assert count.loc["AIRLINE COMPANIES"] == 4
     assert count.loc["AIRLINES"] == 1
     assert count.loc["ANALYTIC APPLICATIONS"] == 9
-    assert count.loc["ANALYTIC MODEL"] == 4
+    assert count.loc["ANALYTIC MODEL"] == 10
